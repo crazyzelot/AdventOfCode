03cbbf8fbda8623a0344c32d6937b7ded1d595cb
crazyzelot <aaron.gordon.personal@gmail.com>
2020-12-18 10:04:19 -0500

    First addition of Day 18

---
+++ 2020/Day18/Part1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func getInput() []string {
+	var inputArray []string
+	file, err := os.Open("./input.txt")
+	check(err)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		check(err)
+		inputArray = append(inputArray, scanner.Text())
+	}
+	return inputArray
+}
+
+func getVInput() []string {
+	var inputArray []string
+	file, err := os.Open("./input.txt")
+	check(err)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		check(err)
+		inputArray = append(inputArray, scanner.Text())
+	}
+	maxValue := len(inputArray)
+	for reader := 0; reader < maxValue; reader++ {
+		fmt.Println(inputArray[reader])
+	}
+	return inputArray
+}
+
+func main() {
+	inputArray := getInput()
+	for c := 0; c < len(inputArray); c++ {
+		fmt.Println(inputArray[c])
+	}
+}
