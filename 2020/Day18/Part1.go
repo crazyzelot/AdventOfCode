4b66834525e43f404c5420fb00f6d39188fc3d27
crazyzelot <aaron.gordon.personal@gmail.com>
2020-12-18 11:03:17 -0500

    Working day one. WOOT WOOT

--- 2020/Day18/Part1.go
+++ 2020/Day18/Part1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func check(e error) {
@@ -42,7 +43,91 @@ func getVInput() []string {
 
 func main() {
 	inputArray := getInput()
+	solution := 0
 	for c := 0; c < len(inputArray); c++ {
-		fmt.Println(inputArray[c])
+		solution = solution + parsing(inputArray[c])
 	}
+	fmt.Println(solution)
+}
+
+func parsing(input string) int {
+	var parsedArray []string
+	solution := 0
+	for i := 0; i < len(input); i++ {
+		if string(input[i]) == "(" {
+			parsedArray = append(parsedArray, "(")
+		} else if string(input[i]) == ")" {
+			parsedArray = append(parsedArray, ")")
+		} else if string(input[i]) == "+" {
+			parsedArray = append(parsedArray, "+")
+		} else if string(input[i]) == "*" {
+			parsedArray = append(parsedArray, "*")
+		} else if string(input[i]) != " " {
+			parsedArray = append(parsedArray, string(input[i]))
+		}
+	}
+	index := 0
+	solution, index = badMaths(parsedArray, index)
+	return solution
+}
+
+func badMaths(parsedArray []string, index int) (int, int) {
+	solution := 0
+	for index < len(parsedArray) {
+		fmt.Println(parsedArray)
+		if parsedArray[index] == "(" {
+			fmt.Println("I found a (")
+			index = index + 1
+			tmp := 0
+			tmp, index = badMaths(parsedArray, index)
+			solution = solution + tmp
+		} else if parsedArray[index] == ")" {
+			fmt.Println("I found a )")
+			index++
+			return solution, index
+		} else {
+			if solution == 0 {
+				fmt.Print("Loading my first element: ")
+				temp, _ := strconv.Atoi(parsedArray[index])
+				solution = temp
+				fmt.Println(solution)
+				index++
+			}
+			if parsedArray[index] == "+" {
+				fmt.Println("I found a +", solution)
+				index++
+				if parsedArray[index] == "(" {
+					fmt.Println("I found a (", solution)
+					index = index + 1
+					tmp := 0
+					tmp, index = badMaths(parsedArray, index)
+					solution = solution + tmp
+					fmt.Println("I now have solution: ", solution, index)
+
+				} else {
+					fmt.Println(parsedArray[index])
+					temp, _ := strconv.Atoi(parsedArray[index])
+					solution = solution + temp
+					index++
+				}
+			} else if parsedArray[index] == "*" {
+				fmt.Println("I found a *")
+				index++
+				if parsedArray[index] == "(" {
+					fmt.Println("I found a (")
+					index = index + 1
+					tmp := 0
+					tmp, index = badMaths(parsedArray, index)
+					solution = solution * tmp
+				} else {
+					fmt.Println(parsedArray[index])
+					temp, _ := strconv.Atoi(parsedArray[index])
+					solution = solution * temp
+					fmt.Println("I just finished Multipling and now have this is a solution: ", solution)
+					index++
+				}
+			}
+		}
+	}
+	return solution, index
 }
