b841440902971d98d97c8096a17551af477437c1
crazyzelot <aaron.gordon.personal@gmail.com>
2020-12-18 11:33:37 -0500

    Creating branch for work done on Day 15.\

---
+++ 2020/Day15/Part1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := "0,3,6"
+	temp := strings.Split(input, ",")
+	var game []int
+	spoken := make(map[int]int)
+	for i := range temp {
+		tmp, _ := strconv.Atoi(temp[i])
+		game = append(game, tmp)
+	}
+	fmt.Println(game)
+	for i := 0; i < 6; i++ {
+		index := i % 3
+		fmt.Println("Number spoken", game[index])
+		_, found := spoken[game[index]]
+		if found {
+
+		} else {
+			fmt.Println("here")
+			spoken[game[index]] = i
+		}
+	}
+}
